4a5a8d3ba623615ec8b0db8feb50b2456083a8ee
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-02-26 22:39:09 +0100

    fix: update kernel and nvidia

--- hosts/desktop/nvidia.nix
+++ hosts/desktop/nvidia.nix
@@ -1,4 +1,4 @@
-{pkgsUnstable, ...}: {
+{pkgs, config, ...}: {
   hardware.graphics = {
     enable = true;
   };
@@ -10,9 +10,7 @@
     nvidiaSettings = true;
   };
 
-  # Avoid nvidia driver from crashing the system
-  boot.kernelParams = ["pcie_aspm=off"];
-
   # To fix https://forums.developer.nvidia.com/t/ubuntu-24-04-unable-to-change-power-state-from-d3cold-to-d0-device-inaccessible/304459
-  boot.kernelPackages = pkgsUnstable.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
 }
