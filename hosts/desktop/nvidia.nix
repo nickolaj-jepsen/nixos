d0e1d787d094905e57352873179bea0ca3f88055
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-02-26 19:21:40 +0100

    fix: nvidia crash on desktop

--- hosts/desktop/nvidia.nix
+++ hosts/desktop/nvidia.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgsUnstable, ...}: {
   hardware.graphics = {
     enable = true;
   };
@@ -12,4 +12,7 @@ _: {
 
   # Avoid nvidia driver from crashing the system
   boot.kernelParams = ["pcie_aspm=off"];
+
+  # To fix https://forums.developer.nvidia.com/t/ubuntu-24-04-unable-to-change-power-state-from-d3cold-to-d0-device-inaccessible/304459
+  boot.kernelPackages = pkgsUnstable.linuxPackages_latest;
 }
