9548960d6a8ec1f149285f174aca7d73d4355e40
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-03-07 08:25:46 +0100

    chore: update git config

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -9,9 +9,19 @@
     enable = true;
     userEmail = "nickolaj@fireproof.website";
     userName = "Nickolaj Jepsen";
+    extraConfig = {
+      gpg.format = "ssh";
+      gpg.ssh.program = "op-ssh-sign";
+      push.autosetupremote = "true";
+      pull.rebase = "true";
+      rerere.enabled = true;
+      init.defaultBranch = "main";
+    };
+    delta.enable = true;
+
     includes = [
       {
-        condition = "hasconfig:remote.*.url:*Digital-Udvikling*";
+        condition = "hasconfig:remote.*.url:git@github.com:Digital-Udvikling/**";
         contents = {
           user.email = "nij@ao.dk";
         };
