81ca8f2403675797fc2b226c6be4a78f42f14074
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-03-12 16:41:46 +0100

    fix: use hyprland nvidia override for desktop

--- hosts/desktop/nvidia.nix
+++ hosts/desktop/nvidia.nix
@@ -19,4 +19,14 @@
 
   # To fix https://forums.developer.nvidia.com/t/ubuntu-24-04-unable-to-change-power-state-from-d3cold-to-d0-device-inaccessible/304459
   boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  fireproof.home-manager.wayland.windowManager.hyprland.settings = {
+    env = [
+      "LIBVA_DRIVER_NAME,nvidia"
+      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+      "NVD_BACKEND,direct"
+    ];
+
+    cursor.no_hardware_cursors = true;
+  };
 }
