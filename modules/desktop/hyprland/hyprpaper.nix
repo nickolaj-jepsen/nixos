67c81de5d133e48dc35ca5708979bd53e86435f2
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-03-12 08:00:05 +0100

    chore: update background

--- modules/desktop/hyprland/hyprpaper.nix
+++ modules/desktop/hyprland/hyprpaper.nix
@@ -5,19 +5,21 @@
 }: let
   background = pkgs.stdenvNoCC.mkDerivation {
     pname = "desktop-background";
-    version = "0.1";
+    version = "0.2";
 
     src = lib.fileset.toSource {
       root = ./.;
       fileset = lib.fileset.unions [
-        ./background.svg
+        ./backgrounds/geometry.svg
+        ./backgrounds/unknown.svg
       ];
     };
 
     nativeBuildInputs = [pkgs.inkscape];
 
     buildPhase = ''
-      inkscape -w 3840 -h 2160 background.svg -o background.png
+      inkscape -w 3840 -h 2160 backgrounds/geometry.svg -o geometry.png
+      inkscape -w 3840 -h 2160 backgrounds/unknown.svg -o unknown.png
     '';
 
     installPhase = ''
@@ -25,14 +27,17 @@
       cp *.svg *.png $out/share/backgrounds
     '';
   };
-  png = background + "/share/backgrounds/background.png";
+  pngs = [
+    (background + "/share/backgrounds/unknown.png")
+    (background + "/share/backgrounds/geometry.png")
+  ];
 in {
   fireproof.home-manager = {
     services.hyprpaper = {
       enable = true;
       settings = {
-        preload = [png];
-        wallpaper = [",${png}"];
+        preload = pngs;
+        wallpaper = [",${builtins.head pngs}"];
       };
     };
   };
