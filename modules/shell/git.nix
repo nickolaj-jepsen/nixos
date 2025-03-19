753f94f4ff17c7cfbacbf2500926c6e39bee9044
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-03-19 09:12:30 +0100

    feat(git): add fixup alias

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{pkgs, lib, ...}: {
   environment.systemPackages = with pkgs; [
     git
     gh
@@ -7,17 +7,25 @@
 
   fireproof.home-manager.programs.git = {
     enable = true;
+
     userEmail = "nickolaj@fireproof.website";
     userName = "Nickolaj Jepsen";
+
     extraConfig = {
       gpg.format = "ssh";
       gpg.ssh.program = "op-ssh-sign";
       push.autosetupremote = "true";
       pull.rebase = "true";
+      rebase.autosqush = "true";
       rerere.enabled = true;
       init.defaultBranch = "main";
     };
+
     delta.enable = true;
+
+    aliases = {
+      "fixup" = "!git log -n 50 --pretty=format:'%h %s' --no-merges | ${lib.getExe pkgs.fzf} | cut -c -7 | xargs -o git commit --fixup";
+    };
 
     includes = [
       {
