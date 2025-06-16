043af310ff0db4b1ce9d073a022e16286b8b934d
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-06-16 11:45:47 +0200

    chore: update pycharm setup

--- legacy_modules/apps/pycharm.nix
+++ legacy_modules/apps/pycharm.nix
@@ -1,10 +1,19 @@
-{pkgsUnstable, ...}: {
+{pkgsUnstable, ...}:
+let 
+  pycharmPkg = pkgsUnstable.jetbrains.pycharm-professional.override {
+    # -Dide.browser.jcef.enabled causes crashes on wayland
+    vmopts = ''
+      -Dide.browser.jcef.enabled=false
+      -Dawt.toolkit.name=WLToolkit
+      -Xmx8G
+    '';
+  };
+  # pycharmWithPlugins = pkgsUnstable.jetbrains.plugins.addPlugins pycharmPkg [
+  #   "github-copilot"
+  # ];
+
+in {
   environment.systemPackages = [
-    (pkgsUnstable.jetbrains.pycharm-professional.override {
-      vmopts = ''
-        -Dawt.toolkit.name=WLToolkit
-        -Xmx8G
-      '';
-    })
+    pycharmPkg
   ];
 }
