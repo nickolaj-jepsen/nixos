e9c4658fa6e7c27abaa7062006d7349ab2daece5
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-07-11 09:32:59 +0200

    chore: update

--- legacy_modules/dev/playwright.nix
+++ legacy_modules/dev/playwright.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}: {
+{pkgsUnstable, ...}: {
   fireproof.home-manager.programs.fish.shellInit = ''
-    set -xg PLAYWRIGHT_BROWSERS_PATH ${pkgs.playwright-driver.browsers}
+    set -xg PLAYWRIGHT_BROWSERS_PATH ${pkgsUnstable.playwright-driver.browsers}
     set -xg PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS true
   '';
 }
