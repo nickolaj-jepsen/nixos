5890ba2dddd9080da1765d18d6944438ef4cd4a2
Nickolaj Jepsen <nickolaj@fireproof.website>
2025-08-25 08:07:11 +0200

    fix: autoclose satty after copy

--- legacy_modules/desktop/screenshot.nix
+++ legacy_modules/desktop/screenshot.nix
@@ -5,7 +5,7 @@
 }: let
   screenshotPkg = pkgs.writeShellScriptBin "screenshot" ''
     AREA=$(${lib.getExe pkgs.slurp} -d)
-    ${lib.getExe pkgs.grim} -t ppm -g "$AREA" - | ${lib.getExe pkgs.satty} -f - --initial-tool=arrow --copy-command=${pkgs.wl-clipboard}/bin/wl-copy --action-on-enter="save-to-clipboard" --disable-notifications
+    ${lib.getExe pkgs.grim} -t ppm -g "$AREA" - | ${lib.getExe pkgs.satty} -f - --initial-tool=arrow --early-exit --copy-command=${pkgs.wl-clipboard}/bin/wl-copy --action-on-enter="save-to-clipboard" --disable-notifications
   '';
 in {
   environment.systemPackages = [
