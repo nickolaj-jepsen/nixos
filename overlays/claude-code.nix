218b58daee194d71ed98995483f7d48845fff3ce
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-02-13 08:44:48 +0100

    chore: update flake

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -4,10 +4,10 @@ _: {
       claude-code =
         pkgs.claude-code.overrideAttrs
         (_oldAttrs: rec {
-          version = "2.1.36";
+          version = "2.1.39";
           src = pkgs.fetchzip {
             url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-            hash = "sha256-fRNGD4+fL11gdwWrlHkFkzHt12Sy+sf2fcZrRhYM0d8=";
+            hash = "sha256-NLLiaJkU91ZnEcQUWIAX9oUTt+C5fnWXFFPelTtWmdo=";
           };
         });
     };
