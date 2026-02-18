718d4db76919409139f6576efbe272e7c3d21e9d
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-02-18 09:41:43 +0100

    chore: use claude-code-bin

--- overlays/claude-code.nix
+++ overlays/claude-code.nix
@@ -1,15 +1,18 @@
-_: {
-  perSystem = {pkgs, ...}: {
+{inputs, ...}: {
+  perSystem = {system, ...}: let
+    pkgsUnstable = import inputs.nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
     overlayAttrs = {
-      claude-code =
-        pkgs.claude-code.overrideAttrs
-        (_oldAttrs: rec {
-          version = "2.1.39";
-          src = pkgs.fetchzip {
-            url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-            hash = "sha256-NLLiaJkU91ZnEcQUWIAX9oUTt+C5fnWXFFPelTtWmdo=";
-          };
-        });
+      claude-code = pkgsUnstable.claude-code-bin.overrideAttrs (_oldAttrs: rec {
+        version = "2.1.45";
+        src = pkgsUnstable.fetchurl {
+          url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/linux-x64/claude";
+          sha256 = "e7e847383c4666994707741e8e56abf84dd2430153620d3ba5ff5a849375e4de";
+        };
+      });
     };
   };
 }
