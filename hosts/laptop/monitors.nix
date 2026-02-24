60b937f403192e5a8931b8911d36450aa2a031a0
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-02-24 10:30:56 +0100

    chore: fix laptop config

--- hosts/laptop/monitors.nix
+++ hosts/laptop/monitors.nix
@@ -2,6 +2,12 @@ _: {
   monitors = [
     {
       name = "eDP-1";
+      resolution = {
+        width = 1920;
+        height = 1080;
+      };
+      refreshRate = 60;
+      refreshRateNiri = 60.0;
     }
   ];
 }
