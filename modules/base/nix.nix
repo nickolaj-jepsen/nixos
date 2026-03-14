56f63316bba4dddf7e0cb8afd21db7b12393fe3a
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-03-15 00:29:17 +0100

    perf: add binary cache

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -18,11 +18,13 @@
     substituters = [
       "https://nix-community.cachix.org"
       "https://fnug.cachix.org"
+      "https://niri.cachix.org"
     ];
 
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "fnug.cachix.org-1:SDUeF2nZSbSPOAMNJdYZdoVB+tHdB8UHHcqhEmizeNk="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
