0dea376ec19904aafb6eafc5c005ae93783656a7
Nickolaj Jepsen <nickolaj@fireproof.website>
2026-05-04 10:51:31 +0200

    fix: attempt to fix browser screensharing

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -16,8 +16,15 @@
 
     xdg.portal = {
       enable = true;
-      extraPortals = [pkgs.xdg-desktop-portal-gtk];
-      config.common.default = "gtk";
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gnome
+        pkgs.xdg-desktop-portal-gtk
+      ];
+      config.common = {
+        default = ["gtk"];
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.Screenshot" = "gnome";
+      };
       xdgOpenUsePortal = true;
     };
 
